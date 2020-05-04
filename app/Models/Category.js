d4c86ed700a30c4333cd75249c3f96a98122eb52
Matheus Costa <matheus271200.ms@gmail.com>
2020-05-04 16:54:55 -0300

    Category and Product Models

---
+++ app/Models/Category.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Category extends Model {
+
+    /**
+     * Relacionamento entre categoria e Imagem de destaque
+     */
+
+    image() {
+        return this.belongsTo('App/Models/Image')
+    }
+
+     /**
+      * Relacionamento entre Categoria e Produtos
+      */
+
+    products() {
+        return this.belongsToMany('App/Models/Product')
+    }
+}
+
+module.exports = Category
