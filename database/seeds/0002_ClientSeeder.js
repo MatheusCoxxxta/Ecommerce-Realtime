66eb3cc7539f4e2d8dcaf36c0ccd1012cb0e6025
Matheus Costa <matheus271200.ms@gmail.com>
2020-05-05 17:31:49 -0300

    Correção e bug adminRole

--- database/seeds/0002_ClientSeeder.js
+++ database/seeds/0002_ClientSeeder.js
@@ -17,7 +17,7 @@ const User = use('App/Models/User')
 
 class ClientSeeder {
   async run () {
-    const role = await Role.findBy('slug', 'client'),
+    const role = await Role.findBy('slug', 'client')
     const clients = await Factory.model('App/Models/User').createMany(30)
     await Promise.all(
       clients.map(async client => {
@@ -33,7 +33,7 @@ class ClientSeeder {
     })
 
     const adminRole = await Role.findBy('slug', 'admin')
-    await user.roles().attach([ role.id ])
+    await user.roles().attach([ adminRole.id ])
   }
 }
 
