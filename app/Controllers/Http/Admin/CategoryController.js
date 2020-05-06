625d00846aac825133a312146afc75f1bdb0b8f4
Matheus Costa <matheus271200.ms@gmail.com>
2020-05-06 17:02:25 -0300

    Função index(lista categorias)

--- app/Controllers/Http/Admin/CategoryController.js
+++ app/Controllers/Http/Admin/CategoryController.js
@@ -20,10 +20,21 @@ class CategoryController {
    * @param {View} ctx.view
    * @param {Object} ctx.pagination
    */
-  
+
   async index ({ request, response, view, pagination }) {
 
-    const categories = await Category.query().paginate(pagination.page, pagination.limit)
+    const title = request.input('title')
+
+    const query = Category.query()
+
+    if(title) {
+      query.where('title', 'LIKE', `%${title}%`)
+    }
+
+    const categories = await query.paginate(
+      pagination.page, 
+      pagination.limit
+    )
 
     return response.send(categories)
   }
