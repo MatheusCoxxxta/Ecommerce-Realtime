b5cbe2c8c6743eabdbb93f3b771b7f2cdb71fce0
Matheus Costa <matheus271200.ms@gmail.com>
2020-05-07 20:43:54 -0300

    Criando Helpers de upload de arquivo

--- app/Helpers/index.js
+++ app/Helpers/index.js
@@ -28,4 +28,72 @@ const str_random = async (length = 40) => {
     return string
 }
 
+/**
+ * Move um único arquivo para caminho especificado, se nenhum caminho for especificado
+ * então 'public/uploads' será utilizado.
+ * 
+ * @param { FileJar } file - O arquivo a ser gerenciado
+ * @param { string } path - O caminho por onde o arquivo deve ser movido
+ * @return { Object<FileJar> }
+ */
+
+const manage_single_upload = async (file, path = null) => {
+    path = path ? path : Helpers.publicPath('uploads')
+
+    // gera um hash aleatório
+
+    const random_name = await str_random(30)
+
+    let filename = `${new Date.getTime()} - ${random_name}.${file.subtype}`
+
+    // renomeia o arquivo e move ele para o path
+
+    await file.move(path, {
+        name: filename
+    })
+
+    return file
+}
+
+/**
+ * Move um múltiplos arquivo para caminho especificado, se nenhum caminho for especificado
+ * então 'public/uploads' será utilizado.
+ * 
+ * @param { FileJar } fileJar - O arquivo a ser gerenciado
+ * @param { string } path - O caminho por onde o arquivo deve ser movido
+ * @return { Object } 
+ */
+
+const manage_multiple_uploads = async (fileJar, path = null) => {
+    path = path ? path : Helpers.publicPath('uploads')
+
+    let successes = [], errors = []
+
+    await Promise.all(
+        fileJar.files.map(async (file) => {
+            let random_name = await str_random(30)
+
+            let filename = `${new Date.getTime()} - ${random_name}.${file.subtype}`
+
+            // move o arquivo
+
+            await file.move(path, {
+                name: filename
+            })
+
+            // verificamos se o arquivo foi movido
+
+            if(file.moved()) {
+                successes.push(file)
+            } else {
+                errors.push(file.error())
+            }
+
+        })
+    )
+
+    return { successes, errors }
+}
+
+
 module.exports = { str_random }
