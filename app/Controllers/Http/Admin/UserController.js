59bfc2b366d3ed5493b1f9b268552e87e8ce4298
Matheus Costa <matheus271200.ms@gmail.com>
2020-05-07 18:17:09 -0300

    User controller atualizado

--- app/Controllers/Http/Admin/UserController.js
+++ app/Controllers/Http/Admin/UserController.js
@@ -3,7 +3,7 @@
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
-
+const User = use('App/Models/User')
 /**
  * Resourceful controller for interacting with users
  */
@@ -16,20 +16,25 @@ class UserController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    * @param {View} ctx.view
+   * @param {Object} ctx.pagination
    */
-  async index ({ request, response, view }) {
-  }
+  async index ({ request, response, pagination }) {
+    const name = request.input('name')
+    
+    const query = User.query()
 
-  /**
-   * Render a form to be used for creating a new user.
-   * GET users/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
+    if(name) {
+      query.where('name', 'LIKE', `%${name}%`)
+      query.orWhere('surname', 'LIKE', `%${name}%`)
+      query.orWhere('email', 'LIKE', `%${name}%`)
+    }
+
+    const users = await query.paginate(
+      pagination.page,
+      pagination.limit
+    )
+
+    return response.send(users)
   }
 
   /**
@@ -40,7 +45,24 @@ class UserController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
+
   async store ({ request, response }) {
+    try {
+      const userData = request.only([
+        'name', 
+        'surname', 
+        'email', 
+        'password', 
+        'image_id'
+      ])
+
+      const user = await User.create(userData)
+      return response.status(201).send(user)      
+    } catch (error) {
+      return response.status(400).send({
+        message: 'Não foi possível criar esse usuário!'
+      })
+    }
   }
 
   /**
@@ -52,19 +74,10 @@ class UserController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
-  }
+  async show ({ params: { id }, request, response }) {
+    const user = await User.findOrFail(id)
 
-  /**
-   * Render a form to update an existing user.
-   * GET users/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
+    return response.send(user)
   }
 
   /**
@@ -75,7 +88,27 @@ class UserController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update ({ params: { id }, request, response }) {
+    const user = await User.findOrFail(id)
+
+    try {
+      const userData = request.only([
+        'name', 
+        'surname', 
+        'email', 
+        'password', 
+        'image_id'
+      ])
+
+      user.merge(userData)
+      await user.save()
+
+      return response.status(201).send(user)      
+    } catch (error) {
+      return response.status(400).send({
+        message: 'Não foi possível atualizar esse usuário!'
+      })
+    }
   }
 
   /**
@@ -86,7 +119,18 @@ class UserController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy ({ params: { id }, request, response }) {
+    const user = await User.findOrFail(id)
+
+    try {
+      await user.delete()
+      
+      return response.status(204).send()
+    } catch (error) {
+      return response.status(500).send({
+        message: 'Não foi possível deletar o usuário!'
+      })
+    }
   }
 }
 
