cc98f38e00525d77a7e3bd3611a85ac9fc8f25b5
Matheus Costa <matheus271200.ms@gmail.com>
2020-05-07 17:35:06 -0300

    Product controller finalizado

--- app/Controllers/Http/Admin/ProductController.js
+++ app/Controllers/Http/Admin/ProductController.js
@@ -4,6 +4,7 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+const Product = use('App/Models/Product')
 /**
  * Resourceful controller for interacting with products
  */
@@ -16,8 +17,24 @@ class ProductController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    * @param {View} ctx.view
+   * 
    */
-  async index ({ request, response, view }) {
+  async index ({ request, response, pagination }) {
+
+    const name = request.input('name')
+    
+    const query = Product.query()
+
+    if(name) {
+      query.where('name', 'LIKE', `%${name}%`)
+    }
+
+    const products = await query.paginate(
+      pagination.page,
+      pagination.limit
+    )
+
+    return response.send(products)
   }
 
   /**
@@ -29,6 +46,23 @@ class ProductController {
    * @param {Response} ctx.response
    */
   async store ({ request, response }) {
+
+    try {
+      const { name, description, price, image_id } = request.all()
+
+    const product = await Product.create({ 
+      name, 
+      description, 
+      price, 
+      image_id 
+    })
+
+    return response.status(201).send(product)
+    } catch (error) {
+      return response.status(400).send({ 
+        message: 'Não foi possível criar o produto!'
+      })
+    }
   }
 
   /**
@@ -40,7 +74,10 @@ class ProductController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
+  async show ({ params: { id }, request, response }) {
+    const product = await Product.findOrFail(id)
+
+    return response.send(product)
   }
 
   /**
@@ -51,7 +88,23 @@ class ProductController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update ({ params: { id }, request, response }) {
+    const product = await Product.findOrFail(id)
+
+    try {
+      const { name, description, price, image_id } = request.all()
+
+      product.merge({ name, description, price, image_id })
+
+      await product.save()
+
+      return response.send(product)
+
+    } catch (error) {
+      return response.status(400).send({ 
+        message: 'Não foi possível atualizar o produto!'
+      })
+    }
   }
 
   /**
@@ -62,7 +115,19 @@ class ProductController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy ({ params: { id }, request, response }) {
+    const product = await Product.findOrFail(id)
+
+    try {
+      await product.delete()
+
+      return response.status(204).send()
+    } catch (error) {
+      return response.status(500).send({
+        message: 'Não foi possivel deleter o produto!'
+      })
+    }
+    
   }
 }
 
