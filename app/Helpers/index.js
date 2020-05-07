7361e2481b3ed4f7b85e4485e092065e4d845e22
Matheus Costa <matheus271200.ms@gmail.com>
2020-05-07 20:13:01 -0300

    Criação de Helper de str_random

---
+++ app/Helpers/index.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const crypto = use('crypto')
+const Helpers = use('Helpers')
+
+/**
+ * Generate random string
+ * 
+ * @param { int } length - O tamanho da string que você quer gerar
+ * @return { string } - uma string randomica do tamanho do length
+ */
+
+const str_random = async (length = 40) => {
+
+    let string = ''
+    let len = string.length
+
+    if(len < length) {
+        let size = length - len
+        let bytes = await crypto.randomBytes(size)
+        let buffer = new Buffer(bytes)
+        string += buffer
+        .toString('base64')
+        .replace(/[^a-zA-Z0-9]/g, '')
+        .substr(0, size)
+    }
+
+    return string
+}
+
+module.exports = { str_random }
--- app/Models/PasswordReset.js
+++ app/Models/PasswordReset.js
@@ -3,6 +3,8 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
+const { str_ramdom } = use('App/Helpers')
+
 class PasswordReset extends Model {
     static boot() {
         super.boot()
